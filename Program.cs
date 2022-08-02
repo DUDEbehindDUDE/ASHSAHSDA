9a795cd8f595271138d68ce51bd29af1edb6022b
DUDEbehindDUDE <jnbrooks457@gmail.com>
2022-08-01 21:32:06 -0400

    the bot turns on!

--- Program.cs
+++ Program.cs
@@ -1,2 +1,35 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Discord;
+using Discord.WebSocket;
+
+//[assembly: log4net.Config.XmlConfigurator(Watch = true)]
+
+public class Program
+{
+    //private static readonly log4net.ILog log = log4net.LogManager.GetLogger("sexy");
+
+    private DiscordSocketClient _client;
+    public static Task Main(string[] args) => new Program().MainAsync();
+
+	public async Task MainAsync()
+	{
+        _client = new DiscordSocketClient();
+
+        _client.Log += Log;
+
+        DotNetEnv.Env.Load("./../../../.env");
+        var token = Environment.GetEnvironmentVariable("TOKEN");
+
+        await _client.LoginAsync(TokenType.Bot, token);
+        await _client.StartAsync();
+
+        // Block this task until the program is closed.
+        await Task.Delay(-1);
+    }
+
+	private Task Log(LogMessage msg)
+	{
+		//log.Info(msg.ToString());
+        Console.WriteLine(msg.ToString());
+		return Task.CompletedTask;
+	}
+}

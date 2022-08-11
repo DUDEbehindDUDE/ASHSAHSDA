b1e5f88bf62b299936233327179af41d5bbb846a
035WasTaken <60830475+035WasTaken@users.noreply.github.com>
2022-08-11 17:13:10 -0400

    wow

--- Program.cs
+++ Program.cs
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 ﻿// will fix later
 #pragma warning disable
 
@@ -44,40 +43,4 @@ namespace NetBot
             return Task.CompletedTask;
         }
     }
-=======
-﻿using Discord;
-using Discord.WebSocket;
-
-//[assembly: log4net.Config.XmlConfigurator(Watch = true)]
-
-public class Program
-{
-    //private static readonly log4net.ILog log = log4net.LogManager.GetLogger("sexy");
-
-    private DiscordSocketClient _client;
-    public static Task Main(string[] args) => new Program().MainAsync();
-
-	public async Task MainAsync()
-	{
-        _client = new DiscordSocketClient();
-
-        _client.Log += Log;
-
-        DotNetEnv.Env.Load("./../../../.env");
-        var token = Environment.GetEnvironmentVariable("TOKEN");
-
-        await _client.LoginAsync(TokenType.Bot, token);
-        await _client.StartAsync();
-
-        // Block this task until the program is closed.
-        await Task.Delay(-1);
-    }
-
-	private Task Log(LogMessage msg)
-	{
-		//log.Info(msg.ToString());
-        Console.WriteLine(msg.ToString());
-		return Task.CompletedTask;
-	}
->>>>>>> 9a795cd8f595271138d68ce51bd29af1edb6022b
 }
